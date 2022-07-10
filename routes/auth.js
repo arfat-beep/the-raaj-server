ac6e95eb5e1ecb100ea1aa412ebdb11f7b31d20a
arfat-beep <arfatrahman08@gmail.com>
2022-07-10 19:34:28 +0600

    jwt token added

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../Models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // registration
 router.post("/register", async (req, res) => {
@@ -34,11 +35,22 @@ router.post("/login", async (req, res) => {
       user.password,
       process.env.PASS_SEC
     );
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRECT_KEY,
+      {
+        expiresIn: "3d",
+      }
+    );
     const orginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
     orginalPassword !== req.body.password &&
       res.status(401).json("Wrong credentials !");
     const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    res.status(200).json({ others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
