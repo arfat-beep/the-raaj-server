7cdb2f37a3673630cba5f9592dd7efdfa95457db
arfat-beep <arfatrahman08@gmail.com>
2022-07-10 13:30:37 +0600

    Schema model for mongoose created

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+// excesories
 const dotenv = require("dotenv");
 const app = express();
+const port = process.env.PORT || 5000;
+const useRoute = require("./routes/user");
 dotenv.config();
+// mongoose mongodb connection
 mongoose
   .connect(`${process.env.MONGO_URL}`)
   .then(() => console.log("dbConnected"))
   .catch((err) => console.log(err));
-app.listen(5000, () => {
-  console.log("App is running on 5000");
+app.use(express.json());
+app.use("/api/users", useRoute);
+
+// app listen and port connection
+app.listen(port, () => {
+  console.log("App is running on", port);
 });
