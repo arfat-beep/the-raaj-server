17b65480845fcb27bd5373068f4d021814f76583
arfat-beep <arfatrahman08@gmail.com>
2022-07-10 11:32:09 +0600

    MongoDB added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
-
+dotenv.config();
+mongoose
+  .connect(`${process.env.MONGO_URL}`)
+  .then(() => console.log("dbConnected"))
+  .catch((err) => console.log(err));
 app.listen(5000, () => {
   console.log("App is running on 5000");
 });
