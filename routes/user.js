4f440989af2cf2b08c1f7d16865b1880b6730410
arfat-beep <arfatrahman08@gmail.com>
2022-07-11 22:09:55 +0600

    get all users and get new 5 users added

--- routes/user.js
+++ routes/user.js
@@ -49,4 +49,17 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// Get all user
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
