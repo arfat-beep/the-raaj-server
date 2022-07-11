88ee6568566ccf5adb56f4066365e33db0aebf22
arfat-beep <arfatrahman08@gmail.com>
2022-07-11 14:13:18 +0600

    update user authentication complete

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,28 @@
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+const CryptoJS = require("crypto-js");
+const User = require("../Models/User");
+
 const router = require("express").Router();
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updateUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
