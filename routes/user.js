32b3a66c7ff41128d837dace428bfe92d79cf54a
arfat-beep <arfatrahman08@gmail.com>
2022-07-11 20:19:46 +0600

    delete user added

--- routes/user.js
+++ routes/user.js
@@ -25,4 +25,14 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+// delete
+router.delete("/id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has beed deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
